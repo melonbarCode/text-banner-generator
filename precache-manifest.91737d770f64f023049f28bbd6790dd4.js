7da54d9a095e8427ba6a48bc6d66adb29955d7f9
melonBarCode <melonbarcode@gmail.com>
2021-02-05 02:05:37 +0900

    Updates

---
+++ precache-manifest.91737d770f64f023049f28bbd6790dd4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72dfab942de4dda24a458e859fda70fc",
+    "url": "/text-banner-generator/index.html"
+  },
+  {
+    "revision": "cc93bcbadde4ee2387dc",
+    "url": "/text-banner-generator/static/css/2.39fabea5.chunk.css"
+  },
+  {
+    "revision": "73adfc018c10834f44c5",
+    "url": "/text-banner-generator/static/css/main.a083d0a7.chunk.css"
+  },
+  {
+    "revision": "cc93bcbadde4ee2387dc",
+    "url": "/text-banner-generator/static/js/2.599cb9ec.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/text-banner-generator/static/js/2.599cb9ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73adfc018c10834f44c5",
+    "url": "/text-banner-generator/static/js/main.9ff34f7f.chunk.js"
+  },
+  {
+    "revision": "cf225ff00bdcb6fae00b",
+    "url": "/text-banner-generator/static/js/runtime-main.201c0714.js"
+  }
+]);
